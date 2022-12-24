d37e9ff7d1abcb261ba469e53fe7019fcb57b63a
Szaneron <Szaneron@gmail.com>
2022-12-24 13:24:45 +0100

    commit

--- sekcja17 - Funkcje genreujące/funkcjegenerujace.py
+++ sekcja17 - Funkcje genreujące/funkcjegenerujace.py
@@ -1,6 +1,8 @@
 """
   yield - z ang. dostarczyć, dać, wydać z siebie
 """
+
+
 def generate_even_numbers():
     print("start")
     for element in range(400):
@@ -9,18 +11,20 @@ def generate_even_numbers():
             yield element
             print("po yield")
 
+
 evenNumbersGenerator = (element
                         for element in range(400)
                         if (element % 2 == 0))
 
-
 a = generate_even_numbers()
+
 
 def generate_10_numbers():
     x = 0
     while x < 10:
         yield x
         x = x + 1
+
 
 print(list(generate_10_numbers()))
 print(list(generate_10_numbers()))
